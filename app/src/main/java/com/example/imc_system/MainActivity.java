7ef89e83b0c6bcce8860e08af03b0e6774abe86c
Mohamed28 <mohamed28.lucas@gmail.com>
2020-04-20 03:25:54 -0300

    First Commit

---
+++ app/src/main/java/com/example/imc_system/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.imc_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static java.lang.String.format;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText inputHeight, inputWeight;
+    private TextView fieldResult, lblResult;
+    private Button btnCalc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inputHeight = (EditText) findViewById(R.id.inputHeight);
+        inputWeight = (EditText) findViewById(R.id.inputWeight);
+        fieldResult = (TextView) findViewById(R.id.fieldResult);
+        lblResult = (TextView) findViewById(R.id.lblResult);
+        btnCalc = (Button) findViewById(R.id.btnCalc);
+
+        btnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calc(view);
+            }
+        });
+    }
+
+    public void calc(View view) {
+        int resultColor = 0;
+        String resultMessage = "";
+        double altura = Double.parseDouble(inputHeight.getText().toString());
+        double peso = Double.parseDouble(inputWeight.getText().toString());
+        double result = Math.round(peso / Math.pow(altura, 2));
+
+        if (result < 18) {
+            resultMessage = "Você está MAGRELO!";
+            resultColor = Color.YELLOW;
+        } else if (result > 18 && result < 25) {
+            resultMessage = "Você está OK!";
+            resultColor = Color.GREEN;
+        } else {
+            resultMessage = "Você está GORDO";
+            resultColor = Color.RED;
+        }
+
+        lblResult.setText(resultMessage);
+        lblResult.setTextColor(resultColor);
+        fieldResult.setText(String.valueOf(result));
+        fieldResult.setTextColor(resultColor);
+    }
+
+}
